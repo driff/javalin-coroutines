fc524316dd6444cffa825d9bc1f3a06d104e789e
Johan Garcia <john.jgp@gmail.com>
2019-03-30 12:07:44 -0500

    1st commit, base hello worldish demo

--- src/main/kotlin/com/treefuerza/pruebas/Start.kt
+++ src/main/kotlin/com/treefuerza/pruebas/Start.kt
@@ -3,9 +3,6 @@ package com.treefuerza.pruebas
 import com.treefuerza.pruebas.endpoints.Products
 import io.javalin.Javalin
 import kotlinx.coroutines.runBlocking
-import sun.plugin2.util.PojoUtil.toJson
-import io.javalin.json.FromJsonMapper
-import io.javalin.json.ToJsonMapper
 
 
 suspend fun main(){
